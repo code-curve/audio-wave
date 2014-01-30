2cba8de768e2870cd00c228f3489da4ee8d5307d
Dan Prince <dan@codecurve.net>
2014-01-30 04:27:15 +0000

    Added audio playback

--- public/js/directives/playback.js
+++ public/js/directives/playback.js
@@ -2,20 +2,35 @@ module.exports = function() {
   return {
     restrict:'A',
     templateUrl: 'partials/audio/play',
-    controller: function($scope, collection) {
-      var audioCollection = collection('audio');
+    controller: function($scope, $interval, collection) {
+      var audioCollection, update;
+      
+      audioCollection = collection('audio');
       
       $scope.audio = null;
       $scope.playing = false;
       $scope.progress = 0;
-      
+      $scope.name = '';
+      $scope.duration = '';
+
       audioCollection.on('focus', function(audio) {
-        console.log(audio);
+        
+        // Totally reset the current audio
+        if($scope.playing) {
+          $scope.playing = false;
+          $scope.audio.pause();
+          $scope.progress = 0;
+          delete $scope.audio;
+          $interval.cancel(update);
+        }
+        
+        $scope.name = audio.name;
+        $scope.duration = audio.duration;
         $scope.audio = new Audio('audio/play/' + audio._id);
         $scope.audio.addEventListener('ended', $scope.stop);
-        $scope.audio.addEventListener('progress', $scope.progression);
+        $scope.refreshProgress();
       });
-
+        
       $scope.play = function() {
         $scope.playing = true;
         $scope.audio.play();
@@ -28,16 +43,20 @@ module.exports = function() {
 
       $scope.stop = function() {
         $scope.playing = false;
+        $scope.progress = 0;
         $scope.audio.pause();
         $scope.audio.currentTime = 0;
       };
 
-      $scope.progression = function(e) {
-        var progress = $scope.audio.currentTime / $scope.audio.duration;
-        progress *= 100;
-        $scope.progress = progress;
+      $scope.progression = function() {
+        var progress;
+        progress = $scope.audio.currentTime / $scope.audio.duration;
+        $scope.progress = progress * 100;
       };
-
+      
+      $scope.refreshProgress = function() {
+        update = $interval($scope.progression, 500);
+      };
     }
   }
 }
